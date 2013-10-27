9887fa6920e7d44d37e895f7d7dd5ef6b86bd419
Fergal Daly <fergal@esatclear.ie>
2013-10-27 12:33:12 +0900

    local vars.sh and bash.sh

--- bash.sh
+++ bash.sh
@@ -2,3 +2,7 @@ DIR=~/toolkit/bash/
 for f in $(run-parts --list $DIR); do
   . $f
 done
+
+if [[ -x ~/.local_bash.sh ]]; then
+  . ~/.local_bash.sh
+fi
--- vars.sh
+++ vars.sh
@@ -1,5 +1,8 @@
-date >> /tmp/all
 DIR=~/toolkit/vars/
 for f in $(run-parts --list $DIR); do
   . $f
 done
+
+if [[ -x ~/.local_vars.sh ]]; then
+  . ~/.local_vars.sh
+fi
