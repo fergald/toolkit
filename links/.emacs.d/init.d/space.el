464c62e4c9963bbbafa94acf0358fff3a9ab38aa
Fergal Daly <fergal@esatclear.ie>
2020-06-13 17:27:29 +0900

    set python indent

--- links/.emacs.d/init.d/space.el
+++ links/.emacs.d/init.d/space.el
@@ -2,3 +2,7 @@
 (setq require-final-newline t)
 (setq-default tab-width 2)
 (setq-default indent-tabs-mode nil)
+(add-hook 'python-mode-hook
+          (function (lambda ()
+                      (setq indent-tabs-mode nil
+                            tab-width 2))))
